e4ff292ff95ad70a28001abf5e4db02abc2fc547
Raul Grados <gnr2092@gmail.com>
2019-06-14 10:34:19 -0500

    Load rounds method

--- TrackerUI/TournamentViewerForm.cs
+++ TrackerUI/TournamentViewerForm.cs
@@ -14,17 +14,43 @@ namespace TrackerUI
     public partial class TournamentViewerForm : Form
     {
         private TournamentModel tournament;
+        List<int> rounds = new List<int>();
 
         public TournamentViewerForm(TournamentModel tournamentModel)
         {
             InitializeComponent();
             tournament = tournamentModel;
             LoadFormData();
+            LoadRounds();
         }
 
         private void LoadFormData()
         {
             tournamentNameLabel.Text = tournament.TournamentName;
+        }
+
+        private void WireUpsList()
+        {
+            roundDropDonw.DataSource = null;
+            roundDropDonw.DataSource = rounds;
+        }
+
+        private void LoadRounds()
+        {
+            rounds = new List<int>();
+            rounds.Add(1);
+            int currentRound = 1;
+
+            foreach (List<MatchupModel> matchups in tournament.Rounds)
+            {
+                if (matchups.First().MatchupRound > currentRound)
+                {
+                    currentRound = matchups.First().MatchupRound;
+                    rounds.Add(currentRound);
+                }
+            }
+
+            WireUpsList();
         }
     }
 }
