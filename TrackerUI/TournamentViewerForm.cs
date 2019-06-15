73ce9e14b025f539f35c122d5e08099d236ffd5b
Raul Grados <gnr2092@gmail.com>
2019-06-14 20:43:49 -0500

    Called method UpdateMatchup in ScoreButton

--- TrackerUI/TournamentViewerForm.cs
+++ TrackerUI/TournamentViewerForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TrackerLibrary;
 using TrackerLibrary.Models;
 
 namespace TrackerUI
@@ -212,6 +213,8 @@ namespace TrackerUI
             }
 
             LoadMatchups((int)roundDropDonw.SelectedItem);
+
+            GlobalConfig.Connection.UpdateMatchup(m);
         }
     }
 }
