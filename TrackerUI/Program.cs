80016c3b782ff73ca24fa9e2d4bc00729e81678d
rgrados <gnr2092@gmail.com>
2019-06-16 12:13:08 -0500

    Set form main

--- TrackerUI/Program.cs
+++ TrackerUI/Program.cs
@@ -20,9 +20,7 @@ namespace TrackerUI
 
             // Initialize the database connections
             TrackerLibrary.GlobalConfig.InitializeConnections(DatabaseType.TextFile);
-            Application.Run(new CreateTournamentForm());
-
-            //Application.Run(new TournamentDashboardForm());
+            Application.Run(new TournamentDashboardForm());
         }
     }
 }
