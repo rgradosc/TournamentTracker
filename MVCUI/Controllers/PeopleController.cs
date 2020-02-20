fc3feea284d3fca601b9f0aefa11f754142e3622
Raul Grados <gnr2092@gmail.com>
2020-02-20 08:00:24 -0500

    Add functionality to create people in web project.

---
+++ MVCUI/Controllers/PeopleController.cs
@@ -0,0 +1,46 @@
+﻿namespace MVCUI.Controllers
+{
+    using System.Collections.Generic;
+    using System.Web.Mvc;
+    using TrackerLibrary;
+    using TrackerLibrary.Models;
+
+    public class PeopleController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            List<PersonModel> availableTeamMembers = GlobalConfig.Connection.GetPerson_All();
+
+            return View(availableTeamMembers);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(PersonModel p)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    GlobalConfig.Connection.CreatePerson(p);
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View(p);
+                }
+            }
+            catch
+            {
+                return View(p);
+            }
+        }
+    }
+}
--- TrackerLibrary/Models/PersonModel.cs
+++ TrackerLibrary/Models/PersonModel.cs
@@ -1,5 +1,7 @@
 ﻿namespace TrackerLibrary.Models
 {
+    using System.ComponentModel.DataAnnotations;
+
     /// <summary>
     /// Represents one person.
     /// </summary>
@@ -13,21 +15,35 @@
         /// <summary>
         /// The first name of the person.
         /// </summary>
+        [Display(Name = "Given Name")]
+        [StringLength(100, MinimumLength = 2)]
+        [Required]
         public string FirstName { get; set; }
 
         /// <summary>
         /// The last name of the person.
         /// </summary>
+        [Display(Name = "Last Name")]
+        [StringLength(100, MinimumLength = 2)]
+        [Required]
         public string LastName { get; set; }
 
         /// <summary>
         /// The primary email address of the person.
         /// </summary>
+        [Display(Name = "Email Address")]
+        [StringLength(200, MinimumLength = 6)]
+        [DataType(DataType.EmailAddress)]
+        [Required]
         public string EmailAddress { get; set; }
 
         /// <summary>
         /// The primary cell phone number of the person.
         /// </summary>
+        [Display(Name = "Cellphone Number")]
+        [StringLength(20, MinimumLength = 10)]
+        [DataType(DataType.PhoneNumber)]
+        [Required]
         public string CellphoneNumber { get; set; }
 
         public string FullName 
